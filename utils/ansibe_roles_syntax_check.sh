782e23c8c15fefb9aeba6cabe25a0465791876d7
Hi <test@user.com>
2018-02-09 13:43:53 +0100

    cleanup ansibe_roles_syntax_check

--- utils/ansibe_roles_syntax_check.sh
+++ utils/ansibe_roles_syntax_check.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#check_ansible_file playbook_file ansible_inventory_file
+# we expect the file has the path
+# [x] */*yml
+# [x] */ansible/*yml
+# [x] */ansible/roles/*yml
+# [x] yml w/o ansible => yml check only
+# [x] yml w/ansible => ansible check
+# [x] yml w/ansible and w/roles and w/test directory 
+# -> [x] check w/ansible-playbook --syntax-check -- check
+# -> [o] check link to role
+# -> [o]test/main.yml available
+# -> [o]check role in main.yml
+# [o] test script 
+
 err_report() {
     echo "Error on line $1"
     exit 1
@@ -32,12 +46,9 @@ fi
 
 }
 
-
-
 # from here
 # https://liquidat.wordpress.com/2016/01/21/short-tip-verify-yaml-in-shell-via-python-one-liner/
 check_yml() {
-
     set +x # make sure the command echos
     output=$(python -c 'import yaml,sys;yaml.safe_load(sys.stdin)' <"${1}" >/dev/null)
     result="$?"
@@ -50,106 +61,57 @@ check_yml() {
 }
 
 check_ansible() {
-
     playbook_file="${1}"
     ansible_inventory_file="${2}"
-
     set +x # make sure the command echos
     output=$(/usr/bin/ansible-playbook --syntax-check --check -i "${ansible_inventory_file}" "${path_of_test_yml}")
     result="$?"
     set +x # undo command echoing
     if [ "$result" -ne 0 ]; then
         echo -e "\033[01;33;41mOught please fix your YAML File\033[0m"
-
     else
         echo -e "Your YAML File looks ok $result"
     fi
-
 }
 
 check_ansible_roles () {
-
 playbook_file="${1}"
 ansible_inventory_file="${2}"
-
-
 # found root roles dir
 #/home/trapapa/Projects/ofGitHub/vagrant-ssh/ca-validate-user-and-host/provisioning/ansible/roles/ca-server/tasks/ca-conf.yml
-
 # from here
 # https://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
 path_items=(${playbook_file//\// })
-
-
-
-#readarray -td, path_items <<<"${path},"; declare -p path_items;
-# from here
-# https://stackoverflow.com/questions/10586153/split-string-into-an-array-in-bash
-#readarray -t path_items <<<"${path}"
-
-# path_items=()
-# while [[ $path =~ ([^,]+)(/[ ]+|$) ]]; do
-#     array+=("${BASH_REMATCH[1]}")   # capture the field
-#     i=${#BASH_REMATCH}              # length of field + delimiter
-#     str=${str:i}                    # advance the string by that length
-# done                                # the loop deletes $str, so make a copy if needed
-
-# declare -p path_items
-
-
-
-
-#echo "path_items ${path_items[@]}"
-echo "path_items $*{path_items}"
-
+#TODO debug echo "path_items $*{path_items}"
 position_count=0;
 path_of_roles=""
 for i in "${path_items[@]}"
-do
-
-path_of_roles="${path_of_roles}/${i}"
-    if [ "$i" == "roles" ] ; then
-        echo "Found ${i}"
-        echo "${position_count}"
-        pos_of_match=${position_count}
-    break
+    do
+    path_of_roles="${path_of_roles}/${i}"
+        if [ "$i" == "roles" ] ; then
+            echo "Found ${i}"
+            echo "${position_count}"
+            pos_of_match=${position_count}
+        break
     fi
-
-    
-
     (( position_count++ ))
 done
 
-echo "path_of_roles ${path_of_roles}"
-
-echo "pos_of_match => ${pos_of_match}"
-
-echo "ansible role => ${path_items[${pos_of_match}+1]}"
-
+#TODO debug echo "path_of_roles ${path_of_roles}"
+#TOD debug echo "pos_of_match => ${pos_of_match}"
+echo "found ansible role =>${path_items[${pos_of_match}+1]}"
 role_name=${path_items[${pos_of_match}+1]}
-
 test_yml_path="${role_name}/tests/main.yml"
-
-
-echo "test_yml_path ${test_yml_path}"
-
-
+#TODO debug echo "test_yml_path ${test_yml_path}"
 path_of_test_yml="${path_of_roles}/${test_yml_path}"
-
-echo "path_of_test_yml ${path_of_test_yml}"
-
+#TODO debugecho "path_of_test_yml ${path_of_test_yml}"
 cmd="/usr/bin/ansible-playbook --syntax-check --check -i "${ansible_inventory_file}" "${path_of_test_yml}" "
-
 # from here
 # https://www.cyberciti.biz/tips/shell-scripting-bash-how-to-create-temporary-random-file-name.html
 tmp_cmd_file="$(mktemp /tmp/run-ansible-playbook-check.XXXXXX)"
-
 echo $cmd > ${tmp_cmd_file}
-
 chmod +x ${tmp_cmd_file}
-
 output=$("${tmp_cmd_file}")
-
 result="$?"
     set +x # undo command echoing
     if [ "$result" -ne 0 ]; then
@@ -157,18 +119,13 @@ result="$?"
     else
         echo -e "Your YAML File looks ok $result"
     fi
-
 }
-
 # from here
 # https://stackoverflow.com/questions/4665051/check-if-passed-argument-is-file-or-directory-in-bash
 check_ansible_file() {
-    
-
 playbook_file="${1}"
 ansible_inventory_file="${2}"
-
-    result=$(file "$playbook_file")
+result=$(file "$playbook_file")
     if [[ $result == *"cannot open"* ]]; then
         echo "NO FILE FOUND ($result) "
         exit 1
@@ -188,31 +145,24 @@ ansible_inventory_file="${2}"
             echo " NO ansible and roles found"
             check_yml "${playbook_file}"
         else
-
             echo "No YAML File"
             return 0 
-
         fi
     fi
-
 }
 
+# main
 playbook_file="${1}"
 ansible_inventory_file="${2}"
-
-
 
 if [ -z "${1}" ]; then
         echo "Missing file"
         exit 1
-        
     fi
-
     if [ -z "${2}" ]; then
         echo "Missing inventory"
         echo " Check YAML syntax only"
         check_yml "${playbook_file}"
-        
     else
         echo "check ...."
         check_inventory_file "${ansible_inventory_file}"
@@ -220,21 +170,4 @@ if [ -z "${1}" ]; then
     fi
 
 echo "Ciao ..."    
-
 exit 0
-
-#check_ansible_file "${1}"
-
-# we expect the file has the path
-# */*yml
-# */ansible/*yml
-# or
-# */ansible/roles/*yml
-# yml w/o ansible => yml check only
-# yml w/ansible => ansible check
-# yml w/ansible and w/roles  and w/test directory
-# -> check link to role
-# -> test/main.yml available
-# -> check role in main.yml
-# => ansible roles check
-#
